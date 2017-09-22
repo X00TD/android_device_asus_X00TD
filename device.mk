e5fc4d320e12f7c4e35f121e9a41c1e4e331d797
Yifan Hong <elsk@google.com>
2017-09-22 15:16:18 -0700

    X00TD: uprev to health@2.0 hal.
    Test: adb logcat -e health
    Test: lshal --debug
    
    Change-Id: I3b14f087d95578d034898600592cb6fb357a9958

--- device.mk
+++ device.mk
@@ -210,8 +210,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
