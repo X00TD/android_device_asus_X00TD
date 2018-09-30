67479ea06e47a1d468f8e87027f8e234b6c1f97d
ShivamKumarJha <jha.shivam3@gmail.com>
2018-09-30 09:48:14 +0530

    DNM: Remove unwanted packages
    Change-Id: I2052c920e071a69cec7c83cc8e04ce84c33d146e

--- device.mk
+++ device.mk
@@ -479,3 +479,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 $(call inherit-product, vendor/asus/X00TD/X00TD-vendor.mk)
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages1 \
+    RemovePackages2 \
+    RemovePackages3 \
+    RemovePackages4
